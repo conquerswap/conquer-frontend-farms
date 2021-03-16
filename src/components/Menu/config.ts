f4760b765064b17b66a13a81c10accccd38fb5f7
conquerswap <conquerswap@gmail.com>
2021-03-16 16:05:48 +0000

    First commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,82 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'http://exchange.conquerswap.finance/',
+      },
+      {
+        label: 'Liquidity',
+        href: 'http://exchange.conquerswap.finance/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Armory',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x32556dC6eE696e0a03c6F7993dA17a7245D93007',
+      },
+      /**
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/conquer-swap/',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/conquer-swap',
+      },
+      */
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/conquerswap/",
+      },
+      /**
+      {
+        label: "Docs",
+        href: "https://conquerswap.gitbook.io/conquer-swap/",
+      },
+      {
+        label: "Blog",
+        href: "https://conquer-swap.medium.com/",
+      },
+      */
+    ],
+  },
+  /**
+  {
+    label: 'Audit by Hacken',
+    icon: 'HelmetIcon',
+    href: 'https://www.conquerswap.finance/files/conquerHackenAudit.pdf',
+  },
+  */
+]
+
+export default config
