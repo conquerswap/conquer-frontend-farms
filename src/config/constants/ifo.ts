f0ddd1daff0c7e9ed1e4ffd7af2fdad74487f57d
conquerswap <testcoswap@gmail.com>
2021-03-27 19:03:05 +0000

    fix

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -3,7 +3,7 @@ import { Ifo } from './types'
 const ifos: Ifo[] = [
   {
     id: 'boat',
-    address: '0x0272D3074FFb33cc590C2D0E9f89254b5a8D071',
+    address: '0x0272D3074FFb33cc590C2D0E9f8925c4b5a8D071',
     isActive: true,
     name: 'Conquerswap Token (BOAT)',
     subTitle: '',
