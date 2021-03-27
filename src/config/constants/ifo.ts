1ecaf8d0e70112981b228d0b68383f459a1b2dcb
conquerswap <testcoswap@gmail.com>
2021-03-27 18:56:45 +0000

    test

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -3,17 +3,16 @@ import { Ifo } from './types'
 const ifos: Ifo[] = [
   {
     id: 'boat',
-    address: '0x0272D3074FFb33cc590C2D0E9f8925c4b5a8D071',
+    address: '0x0272D3074FFb33cc590C2D0E9f89254b5a8D071',
     isActive: true,
     name: 'Conquerswap Token (BOAT)',
     subTitle: '',
     description:
       'Conquer Swap is a decentralized exchange running on Binance Smart Chain and Pancake swap exchange, with lots of other features that let you earn and win tokens.',
-    launchDate: 'Mar. 28',
-    launchTime: '9PM SGT',
-    saleAmount: '16,666 BOAT',
-    raiseAmount: '$100,000',
-    cakeToBurn: '$500,000',
+    launchDate: 'Mar. 27',
+    launchTime: '15PM GMT',
+    saleAmount: '8,333 BOAT',
+    raiseAmount: '$50,000',
     projectSiteUrl: 'https://conquerswap.finance',
     currency: 'BUSD LP',
     currencyAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
